fa06075ca87102d0c926415df83e068c22041a9f
Richard Le Mesurier <richard@glowworm.mobi>
2018-05-10 17:30:48 +0200

    Implement solution as per Video 16
    This does not work as the suggested code has a bug in it.
    
    See https://github.com/udacity/ud851-Exercises/issues/70

--- app/src/main/java/com/example/android/implicitintents/MainActivity.java
+++ app/src/main/java/com/example/android/implicitintents/MainActivity.java
@@ -49,11 +49,17 @@ public class MainActivity extends AppCompatActivity {
      */
     public void onClickOpenAddressButton(View v) {
         // TODO (5) Store an address in a String
+        String address = "St James, Cape Town, South Africa";
 
         // TODO (6) Use Uri.Builder with the appropriate scheme and query to form the Uri for the address
+        Uri uri = new Uri.Builder()
+                .scheme("geo")
+                .path("0,0")
+                .query(address)
+                .build();
 
         // TODO (7) Replace the Toast with a call to showMap, passing in the Uri from the previous step
-        Toast.makeText(this, "TODO: Open a map when this button is clicked", Toast.LENGTH_SHORT).show();
+        showMap(uri);
     }
 
     /**
@@ -71,9 +77,8 @@ public class MainActivity extends AppCompatActivity {
      * similar to what I've done above. You can view a list of implicit Intents on the Common
      * Intents page from the developer documentation.
      *
-     * @see <http://developer.android.com/guide/components/intents-common.html/>
-     *
      * @param v Button that was clicked.
+     * @see <http://developer.android.com/guide/components/intents-common.html/>
      */
     public void createYourOwn(View v) {
         Toast.makeText(this,
@@ -113,12 +118,18 @@ public class MainActivity extends AppCompatActivity {
 
 
     // TODO (1) Create a method called showMap with a Uri as the single parameter
-    // Do steps 2 - 4 within the showMap method
+    void showMap(Uri uri) {
+        // Do steps 2 - 4 within the showMap method
         // TODO (2) Create an Intent with action type, Intent.ACTION_VIEW
+        Intent i = new Intent(Intent.ACTION_VIEW);
 
         // TODO (3) Set the data of the Intent to the Uri passed into this method
+        i.setData(uri);
 
         // TODO (4) Verify that this Intent can be launched and then call startActivity
-
+        if (i.resolveActivity(getPackageManager()) != null) {
+            startActivity(i);
+        }
+    }
 
 }
